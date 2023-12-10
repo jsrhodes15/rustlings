b6d18368ba774cbc1b7de3bda3877312f46fb1b3
Jordan Rhodes <jsrhodes15@gmail.com>
2023-12-09 21:39:55 -0500

    Complete generics and traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,17 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-trait AppendBar {
+trait AppendBar<T> {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar<String> for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
